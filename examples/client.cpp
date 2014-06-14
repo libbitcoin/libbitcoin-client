a804166f7bf5d59c5392fe9433f5151f5811f8bb
William Swanson <swansontec@gmail.com>
2014-06-13 17:39:36 -0700

    examples: Add the cli framework

--- examples/client.cpp
+++ examples/client.cpp
@@ -1,6 +1,90 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+#include "read_line.hpp"
+
+/**
+ * Command-line interface for talking to the oblelisk server.
+ */
+class cli
+{
+public:
+    ~cli();
+    cli();
+
+    int run();
+
+private:
+    void command();
+    void cmd_exit();
+    void cmd_help();
+
+    bool done_;
+
+    zmq::context_t context_;
+    read_line terminal_;
+};
+
+cli::~cli()
+{
+}
+
+cli::cli()
+  : done_(false), terminal_(context_)
+{
+}
+
+/**
+ * The main loop for the example application. This loop can be woken up
+ * by either events from the network or by input from the terminal.
+ */
+int cli::run()
+{
+    std::cout << "type \"help\" for instructions" << std::endl;
+    terminal_.show_prompt();
+
+    while (!done_)
+    {
+        auto pollitem = terminal_.pollitem();
+        zmq::poll(&pollitem, 1, -1);
+        command();
+    }
+    return 0;
+}
+
+/**
+ * Reads a command from the terminal thread, and processes it appropriately.
+ */
+void cli::command()
+{
+    std::stringstream reader(terminal_.get_line());
+    std::string command;
+    reader >> command;
+
+    if (command == "exit")              cmd_exit();
+    else if (command == "help")         cmd_help();
+    else
+        std::cout << "unknown command " << command << std::endl;
+
+    // Display another prompt, if needed:
+    if (!done_)
+        terminal_.show_prompt();
+}
+
+void cli::cmd_exit()
+{
+    done_ = true;
+}
+
+void cli::cmd_help()
+{
+    std::cout << "commands:" << std::endl;
+    std::cout << "  exit              - leave the program" << std::endl;
+    std::cout << "  help              - this menu" << std::endl;
+}
 
 int main()
 {
-    std::cout << "test" << std::endl;
+    cli c;
+    return c.run();
 }
---
+++ examples/read_line.cpp
@@ -0,0 +1,61 @@
+#include "read_line.hpp"
+
+#include <cstring>
+#include <iostream>
+
+read_line::~read_line()
+{
+    socket_.send("", 1);
+    thread_->join();
+    delete thread_;
+}
+
+read_line::read_line(zmq::context_t& context)
+  : socket_(context, ZMQ_REQ)
+{
+    socket_.bind("inproc://terminal");
+    // The thread must be constructed after the socket is already bound.
+    // The context must be passed by pointer to avoid copying.
+    auto functor = std::bind(&read_line::run, this , &context);
+    thread_ = new std::thread(functor);
+}
+
+void read_line::show_prompt()
+{
+    std::cout << "> " << std::flush;
+    socket_.send("", 0);
+}
+
+zmq_pollitem_t read_line::pollitem()
+{
+    return zmq_pollitem_t
+    {
+        socket_, 0, ZMQ_POLLIN, 0
+    };
+}
+
+std::string read_line::get_line()
+{
+    char line[1000];
+    size_t size = socket_.recv(line, sizeof(line), ZMQ_DONTWAIT);
+    return std::string(line, size);
+}
+
+void read_line::run(zmq::context_t* context)
+{
+    zmq::socket_t socket(*context, ZMQ_REP);
+    socket.connect("inproc://terminal");
+
+    while (true)
+    {
+        // Wait for a socket request:
+        char request[1];
+        if (socket.recv(request, sizeof(request)))
+            return;
+
+        // Read the input:
+        char line[1000];
+        std::cin.getline(line, sizeof(line));
+        socket.send(line, std::strlen(line));
+    }
+}
