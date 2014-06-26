3e7061bbbea8033ae2e33170c86c7e2453dc8fa5
William Swanson <swansontec@gmail.com>
2014-06-25 17:38:28 -0700

    examples: Try sending messages to the server

--- examples/client.cpp
+++ examples/client.cpp
@@ -3,16 +3,18 @@
 #include <string>
 #include <zmq.hpp>
 #include "read_line.hpp"
+#include <bitcoin/client.hpp>
 
 /**
  * A dynamically-allocated structure holding the resources needed for a
  * connection to a bitcoin server.
  */
 class connection
+  : public libbitcoin::client::message_stream
 {
 public:
     connection(zmq::context_t& context, const std::string& server)
-      : socket(context, ZMQ_DEALER)
+      : socket(context, ZMQ_DEALER), codec(*this)
     {
         // Do not wait for unsent messages when shutting down:
         int linger = 0;
@@ -20,7 +22,16 @@ public:
         socket.connect(server.c_str());
     }
 
+    void message(const libbitcoin::data_chunk& data, bool more)
+    {
+        int flags = 0;
+        if (more)
+            flags = ZMQ_SNDMORE;
+        socket.send(data.data(), data.size(), flags);
+    }
+
     zmq::socket_t socket;
+    libbitcoin::client::obelisk_codec codec;
 };
 
 /**
@@ -40,6 +51,7 @@ private:
     void cmd_help();
     void cmd_connect(std::stringstream& args);
     void cmd_disconnect(std::stringstream& args);
+    void cmd_height(std::stringstream& args);
 
     bool check_connection();
     bool read_string(std::stringstream& args, std::string& out,
@@ -84,7 +96,11 @@ int cli::run()
         if (items[0].revents)
             command();
         if (connection_ && items[1].revents)
-            ; // Server
+        {
+            zmq::message_t msg;
+            connection_->socket.recv(&msg);
+            std::cout << "server reply" << std::endl;
+        }
     }
     return 0;
 }
@@ -102,6 +118,7 @@ void cli::command()
     else if (command == "help")         cmd_help();
     else if (command == "connect")      cmd_connect(reader);
     else if (command == "disconnect")   cmd_disconnect(reader);
+    else if (command == "height")       cmd_height(reader);
     else
         std::cout << "unknown command " << command << std::endl;
 
@@ -122,6 +139,7 @@ void cli::cmd_help()
     std::cout << "  help              - this menu" << std::endl;
     std::cout << "  connect <server>  - connect to a server" << std::endl;
     std::cout << "  disconnect        - disconnect from the server" << std::endl;
+    std::cout << "  height            - get last block height" << std::endl;
 }
 
 void cli::cmd_connect(std::stringstream& args)
@@ -149,6 +167,23 @@ void cli::cmd_disconnect(std::stringstream& args)
         return;
     delete connection_;
     connection_ = nullptr;
+}
+
+static void on_error(const std::error_code& ec)
+{
+    std::cout << "error: " << ec.message() << std::endl;
+}
+
+void cli::cmd_height(std::stringstream& args)
+{
+    if (!check_connection())
+        return;
+
+    auto handler = [](size_t height)
+    {
+        std::cout << "height: " << height << std::endl;
+    };
+    connection_->codec.fetch_last_height(on_error, handler);
 }
 
 /**
