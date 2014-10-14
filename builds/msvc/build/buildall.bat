8e9777fa245ca154b700b91db7600566d3e8c158
evoskuil <eric@voskuil.org>
2014-10-14 14:39:17 -0700

    Update nuget paths.

--- builds/msvc/build/buildall.bat
+++ builds/msvc/build/buildall.bat
@@ -1,7 +1,9 @@
 @ECHO OFF
 ECHO.
 ECHO Downloading libitcoin-client dependencies from NuGet
+CALL nuget.exe install ..\vs2013\bitcoin-client\packages.config
 CALL nuget.exe install ..\vs2013\libbitcoin-client\packages.config
+CALL nuget.exe install ..\vs2013\libbitcoin-client-test\packages.config
 ECHO.
 CALL buildbase.bat ..\vs2013\libbitcoin-client.sln 12
 ECHO.
