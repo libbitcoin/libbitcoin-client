e9683ea0155c94a93be66cd6ede692b5341bf880
William Swanson <swansontec@gmail.com>
2014-12-04 02:31:11 -0800

    Split the obelisk codec into two parts
    One part handles the request/reply matching,
    and the other handles the individual message encoding.

--- include/bitcoin/client/obelisk/obelisk_codec.hpp
+++ include/bitcoin/client/obelisk/obelisk_codec.hpp
@@ -21,10 +21,8 @@
 #define LIBBITCOIN_CLIENT_OBELISK_OBELISK_CODEC_HPP
 
 #include <functional>
-#include <map>
 #include <bitcoin/client/define.hpp>
-#include <bitcoin/client/message_stream.hpp>
-#include <bitcoin/client/sleeper.hpp>
+#include <bitcoin/client/obelisk/obelisk_router.hpp>
 #include <bitcoin/client/obelisk/obelisk_types.hpp>
 
 namespace libbitcoin {
@@ -35,18 +33,9 @@ namespace client {
  * This class is a pure codec; it does not talk directly to zeromq.
  */
 class obelisk_codec
-  : public message_stream, public sleeper
+  : public obelisk_router
 {
 public:
-    // Loose message handlers:
-    typedef std::function<void (const std::string& command)> unknown_handler;
-    typedef std::function<void (const payment_address& address, size_t height,
-        const hash_digest& blk_hash, const transaction_type&)> update_handler;
-
-    BCC_API static void on_unknown_nop(const std::string&);
-    BCC_API static void on_update_nop(const payment_address&,
-        size_t, const hash_digest&, const transaction_type&);
-
     /**
      * Constructor.
      * @param out a stream to receive outgoing messages created by the codec.
@@ -64,22 +53,7 @@ public:
 
     BCC_API virtual ~obelisk_codec();
 
-    BCC_API virtual void set_on_update(update_handler on_update);
-    BCC_API virtual void set_on_unknown(unknown_handler on_unknown);
-    BCC_API virtual void set_retries(uint8_t retries);
-    BCC_API virtual void set_timeout(period_ms timeout);
-
-    BCC_API uint64_t outstanding_call_count() const;
-
-    // message-stream interface:
-    BCC_API virtual void write(const data_stack& data) override;
-
-    // sleeper interface:
-    BCC_API virtual period_ms wakeup(bool enable_sideeffects=true) override;
-
     // Message reply handlers:
-    typedef std::function<void (const std::error_code&)>
-        error_handler;
     typedef std::function<void (const history_list&)>
         fetch_history_handler;
     typedef std::function<void (const transaction_type&)>
@@ -140,16 +114,6 @@ public:
 //        const address_prefix& prefix);
 
 private:
-    typedef deserializer<data_chunk::const_iterator, true> data_deserial;
-
-    /**
-     * Decodes a message and calls the appropriate callback.
-     * By the time this is called, the error code has already been read
-     * out of the payload and checked.
-     * If there is something wrong with the payload, this function should
-     * throw a end_of_stream exception.
-     */
-    typedef std::function<void (data_deserial& payload)> decoder;
     static void decode_empty(data_deserial& payload,
         empty_handler& handler);
     static void decode_fetch_history(data_deserial& payload,
@@ -166,65 +130,6 @@ private:
         fetch_stealth_handler& handler);
     static void decode_validate(data_deserial& payload,
         validate_handler& handler);
-
-    /**
-     * Verifies that the deserializer has reached the end of the payload,
-     * and throws end_of_stream if not.
-     */
-    static void check_end(data_deserial& payload);
-
-    /**
-     * Sends an outgoing request, and adds the handlers to the pending
-     * request table.
-     */
-    void send_request(const std::string& command,
-        const data_chunk& payload,
-        error_handler on_error, decoder on_reply);
-
-    struct obelisk_message
-    {
-        std::string command;
-        uint32_t id;
-        data_chunk payload;
-    };
-    void send(const obelisk_message& message);
-    void receive(const obelisk_message& message);
-    void decode_update(const obelisk_message& message);
-    void decode_reply(const obelisk_message& message,
-        error_handler& on_error, decoder& on_reply);
-
-    // Incoming message assembly:
-    obelisk_message wip_message_;
-    enum message_part
-    {
-        command_part,
-        id_part,
-        payload_part,
-        error_part
-    };
-
-    // Request management:
-    uint32_t last_request_id_;
-    struct pending_request
-    {
-        obelisk_message message;
-        error_handler on_error;
-        decoder on_reply;
-        unsigned retries;
-        std::chrono::steady_clock::time_point last_action;
-    };
-    std::map<uint32_t, pending_request> pending_requests_;
-
-    // Timeout parameters:
-    period_ms timeout_;
-    uint8_t retries_;
-
-    // Loose-message event handlers:
-    unknown_handler on_unknown_;
-    update_handler on_update_;
-
-    // Outgoing message stream:
-    std::shared_ptr<message_stream> out_;
 };
 
 } // namespace client
--- src/obelisk/obelisk_codec.cpp
+++ src/obelisk/obelisk_codec.cpp
@@ -30,121 +30,16 @@ BCC_API obelisk_codec::obelisk_codec(
     unknown_handler on_unknown,
     period_ms timeout,
     uint8_t retries)
-  : last_request_id_(0),
-    timeout_(timeout), retries_(retries),
-    on_unknown_(std::move(on_unknown)), on_update_(std::move(on_update)),
-    out_(out)
+  : obelisk_router(out)
 {
+    set_on_update(std::move(on_update));
+    set_on_unknown(std::move(on_unknown));
+    set_timeout(timeout);
+    set_retries(retries);
 }
 
 obelisk_codec::~obelisk_codec()
 {
-}
-
-BCC_API void obelisk_codec::set_on_update(update_handler on_update)
-{
-    on_update_ = std::move(on_update);
-}
-
-BCC_API void obelisk_codec::set_on_unknown(unknown_handler on_unknown)
-{
-    on_unknown_ = std::move(on_unknown);
-}
-
-BCC_API void obelisk_codec::set_retries(uint8_t retries)
-{
-    retries_ = retries;
-}
-
-BCC_API void obelisk_codec::set_timeout(period_ms timeout)
-{
-    timeout_ = timeout;
-}
-
-BCC_API uint64_t obelisk_codec::outstanding_call_count() const
-{
-    return pending_requests_.size();
-}
-
-BCC_API void obelisk_codec::write(const data_stack& data)
-{
-    if (data.size() == 3)
-    {
-        bool success = true;
-        obelisk_message message;
-
-        auto it = data.begin();
-
-        if (success)
-        {
-            // read command
-            message.command = std::string((*it).begin(), (*it).end());
-            it++;
-        }
-
-        if (success)
-        {
-            // read id
-            if ((*it).size() == sizeof(uint32_t))
-            {
-                message.id = from_little_endian_unsafe<uint32_t>(
-                    (*it).begin());
-            }
-            else
-            {
-                success = false;
-            }
-
-            it++;
-        }
-
-        if (success)
-        {
-            // read payload
-            message.payload = (*it);
-            it++;
-        }
-
-        receive(message);
-    }
-}
-
-BCC_API period_ms obelisk_codec::wakeup(bool enable_sideeffects)
-{
-    period_ms next_wakeup(0);
-    auto now = std::chrono::steady_clock::now();
-
-    auto i = pending_requests_.begin();
-    while (i != pending_requests_.end())
-    {
-        auto request = i++;
-        auto elapsed = std::chrono::duration_cast<period_ms>(
-            now - request->second.last_action);
-        if (timeout_ <= elapsed)
-        {
-            if (enable_sideeffects)
-            {
-                if (request->second.retries < retries_)
-                {
-                    // Resend:
-                    ++request->second.retries;
-                    request->second.last_action = now;
-                    next_wakeup = min_sleep(next_wakeup, timeout_);
-                    send(request->second.message);
-                }
-                else
-                {
-                    // Cancel:
-                    auto ec = std::make_error_code(std::errc::timed_out);
-                    request->second.on_error(ec);
-                    pending_requests_.erase(request);
-                }
-            }
-        }
-        else
-            next_wakeup = min_sleep(next_wakeup, timeout_ - elapsed);
-    }
-    return next_wakeup;
 }
 
 BCC_API void obelisk_codec::fetch_history(error_handler on_error,
@@ -450,111 +345,6 @@ void obelisk_codec::decode_validate(data_deserial& payload,
         unconfirmed.push_back(unconfirm_index);
     }
     handler(unconfirmed);
-}
-
-void obelisk_codec::check_end(data_deserial& payload)
-{
-    if (payload.iterator() != payload.end())
-        throw end_of_stream();
-}
-
-void obelisk_codec::send_request(const std::string& command,
-    const data_chunk& payload,
-    error_handler on_error, decoder on_reply)
-{
-    uint32_t id = ++last_request_id_;
-    pending_request& request = pending_requests_[id];
-    request.message = obelisk_message{command, id, payload};
-    request.on_error = std::move(on_error);
-    request.on_reply = std::move(on_reply);
-    request.retries = 0;
-    request.last_action = std::chrono::steady_clock::now();
-    send(request.message);
-}
-
-void obelisk_codec::send(const obelisk_message& message)
-{
-    if (out_)
-    {
-        data_stack data;
-        data.push_back(to_data_chunk(message.command));
-        data.push_back(to_data_chunk(to_little_endian(message.id)));
-        data.push_back(message.payload);
-        out_->write(data);
-    }
-}
-
-void obelisk_codec::receive(const obelisk_message& message)
-{
-    if ("address.update" == message.command)
-    {
-        decode_update(message);
-        return;
-    }
-
-    auto i = pending_requests_.find(message.id);
-    if (i == pending_requests_.end())
-    {
-        on_unknown_(message.command);
-        return;
-    }
-    decode_reply(message, i->second.on_error, i->second.on_reply);
-    pending_requests_.erase(i);
-}
-
-void obelisk_codec::decode_update(const obelisk_message& message)
-{
-    data_deserial deserial = make_deserializer(
-        message.payload.begin(), message.payload.end());
-    try
-    {
-        // This message does not have an error_code at the beginning.
-        uint8_t version_byte = deserial.read_byte();
-        short_hash addr_hash = deserial.read_short_hash();
-        payment_address address(version_byte, addr_hash);
-        uint32_t height = deserial.read_4_bytes();
-        hash_digest blk_hash = deserial.read_hash();
-        transaction_type tx;
-        satoshi_load(deserial.iterator(), deserial.end(), tx);
-        deserial.set_iterator(deserial.iterator() + satoshi_raw_size(tx));
-        check_end(deserial);
-        on_update_(address, height, blk_hash, tx);
-    }
-    catch (end_of_stream)
-    {
-        on_unknown_(message.command);
-    }
-}
-
-void obelisk_codec::decode_reply(const obelisk_message& message,
-    error_handler& on_error, decoder& on_reply)
-{
-    std::error_code ec;
-    data_deserial deserial = make_deserializer(
-        message.payload.begin(), message.payload.end());
-    try
-    {
-        uint32_t val = deserial.read_4_bytes();
-        if (val)
-            ec = static_cast<error::error_code_t>(val);
-        else
-            on_reply(deserial);
-    }
-    catch (end_of_stream)
-    {
-        ec = std::make_error_code(std::errc::bad_message);
-    }
-    if (ec)
-        on_error(ec);
-}
-
-BCC_API void obelisk_codec::on_unknown_nop(const std::string&)
-{
-}
-
-BCC_API void obelisk_codec::on_update_nop(const payment_address&,
-    size_t, const hash_digest&, const transaction_type&)
-{
 }
 
 } // namespace client
