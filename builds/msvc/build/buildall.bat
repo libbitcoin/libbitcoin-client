20bfd0ddc47e1b4a942f9aa833d1e354a6b76bd0
evoskuil <eric@voskuil.org>
2015-07-07 13:37:06 -0700

    Update VS build script for path change.

--- builds/msvc/build/buildall.bat
+++ builds/msvc/build/buildall.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 ECHO.
 ECHO Downloading libbitcoin-client dependencies from NuGet
-CALL nuget.exe install ..\vs2013\example-client\packages.config
 CALL nuget.exe install ..\vs2013\libbitcoin-client\packages.config
+CALL nuget.exe install ..\vs2013\libbitcoin-client-examples\packages.config
 CALL nuget.exe install ..\vs2013\libbitcoin-client-test\packages.config
 ECHO.
 CALL buildbase.bat ..\vs2013\libbitcoin-client.sln 12
