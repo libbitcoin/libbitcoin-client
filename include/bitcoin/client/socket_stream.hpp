1855193083f019e4a60bba57fad8dee1c450554b
evoskuil <eric@voskuil.org>
2016-05-06 13:43:30 -0700

    Whitespace.

--- include/bitcoin/client/socket_stream.hpp
+++ include/bitcoin/client/socket_stream.hpp
@@ -17,7 +17,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 #ifndef LIBBITCOIN_CLIENT_SOCKET_STREAM_HPP
 #define LIBBITCOIN_CLIENT_SOCKET_STREAM_HPP
 
