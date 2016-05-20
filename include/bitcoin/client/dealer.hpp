82890519d1e378bdf938f1a40154ace71df2cca3
evoskuil <eric@voskuil.org>
2016-05-20 03:09:59 -0700

    Comments.

--- include/bitcoin/client/dealer.hpp
+++ include/bitcoin/client/dealer.hpp
@@ -72,7 +72,7 @@ public:
     /// Read from this stream onto the specified stream.
     virtual bool read(stream& stream) override;
 
-    /// Read the spcified data to this stream.
+    /// Write the specified data to this stream.
     virtual void write(const data_stack& data) override;
 
 protected:
