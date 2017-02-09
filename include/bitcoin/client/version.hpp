3070e7abe7d024099f7b23adc24b7dd295cd0cb6
evoskuil <eric@voskuil.org>
2017-02-09 14:35:24 -0800

    Bump master branch from version 3.0.0 to version 4.0.0.

--- include/bitcoin/client/version.hpp
+++ include/bitcoin/client/version.hpp
@@ -12,8 +12,8 @@
  * For interpretation of the versioning scheme see: http://semver.org
  */
 
-#define LIBBITCOIN_CLIENT_VERSION "3.0.0"
-#define LIBBITCOIN_CLIENT_MAJOR_VERSION 3
+#define LIBBITCOIN_CLIENT_VERSION "4.0.0"
+#define LIBBITCOIN_CLIENT_MAJOR_VERSION 4
 #define LIBBITCOIN_CLIENT_MINOR_VERSION 0
 #define LIBBITCOIN_CLIENT_PATCH_VERSION 0
 
