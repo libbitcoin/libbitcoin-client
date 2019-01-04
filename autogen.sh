8d908cc8d3714443999b94888f5022713b99c413
Phillip Mienk <mienkphi@gmail.com>
2019-01-04 15:34:26 -0800

    Update copyright year.

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ###############################################################################
-#  Copyright (c) 2014-2018 libbitcoin-client developers (see COPYING).
+#  Copyright (c) 2014-2019 libbitcoin-client developers (see COPYING).
 #
 #         GENERATED SOURCE CODE, DO NOT EDIT EXCEPT EXPERIMENTALLY
 #
