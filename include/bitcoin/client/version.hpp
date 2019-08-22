3010dfc0a4a0f9d25799f068963b891ba11d8119
Neill Miller <neillm@thecodefactory.org>
2019-08-21 21:13:16 -0400

    Regenerate build artifacts.

--- include/bitcoin/client/version.hpp
+++ include/bitcoin/client/version.hpp
@@ -12,9 +12,9 @@
  * For interpretation of the versioning scheme see: http://semver.org
  */
 
-#define LIBBITCOIN_CLIENT_VERSION "3.4.0"
+#define LIBBITCOIN_CLIENT_VERSION "3.6.0"
 #define LIBBITCOIN_CLIENT_MAJOR_VERSION 3
-#define LIBBITCOIN_CLIENT_MINOR_VERSION 4
+#define LIBBITCOIN_CLIENT_MINOR_VERSION 6
 #define LIBBITCOIN_CLIENT_PATCH_VERSION 0
 
 #endif
