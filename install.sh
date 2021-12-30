19274464ff9715d7e839049966cfa34514313fe8
evoskuil <eric@voskuil.org>
2021-12-30 13:09:19 -0800

    Update CI artifacts.

--- install.sh
+++ install.sh
@@ -117,7 +117,7 @@ make_jobs()
     shift 1
 
     SEQUENTIAL=1
-    # Avoid setting -j1 (causes problems on Travis).
+    # Avoid setting -j1 (causes problems on single threaded systems [TRAVIS]).
     if [[ $JOBS > $SEQUENTIAL ]]; then
         make -j"$JOBS" "$@"
     else
@@ -701,8 +701,8 @@ build_from_local()
     make_current_directory "$JOBS" "${CONFIGURATION[@]}"
 }
 
-# Because Travis alread has downloaded the primary repo.
-build_from_travis()
+# Because continuous integration services has downloaded the primary repository.
+build_from_ci()
 {
     local ACCOUNT=$1
     local REPO=$2
@@ -711,9 +711,9 @@ build_from_travis()
     local OPTIONS=$5
     shift 5
 
-    # The primary build is not downloaded if we are running in Travis.
-    if [[ $TRAVIS == true ]]; then
-        build_from_local "Local $TRAVIS_REPO_SLUG" "$JOBS" "${OPTIONS[@]}" "$@"
+    # The primary build is not downloaded if we are running on a continuous integration system.
+    if [[ $CI == true ]]; then
+        build_from_local "Local $CI_REPOSITORY" "$JOBS" "${OPTIONS[@]}" "$@"
         make_tests "$JOBS"
     else
         build_from_github "$ACCOUNT" "$REPO" "$BRANCH" "$JOBS" "${OPTIONS[@]}" "$@"
@@ -735,7 +735,7 @@ build_all()
     build_from_github libbitcoin secp256k1 version7 "$PARALLEL" "${SECP256K1_OPTIONS[@]}" "$@"
     build_from_github libbitcoin libbitcoin-system master "$PARALLEL" "${BITCOIN_SYSTEM_OPTIONS[@]}" "$@"
     build_from_github libbitcoin libbitcoin-protocol master "$PARALLEL" "${BITCOIN_PROTOCOL_OPTIONS[@]}" "$@"
-    build_from_travis libbitcoin libbitcoin-client master "$PARALLEL" "${BITCOIN_CLIENT_OPTIONS[@]}" "$@"
+    build_from_ci libbitcoin libbitcoin-client master "$PARALLEL" "${BITCOIN_CLIENT_OPTIONS[@]}" "$@"
 }
 
 
