bd1ed53780a3a8b06b06a2fe6dc27bdfa3e881fd
Phillip Mienk <mienkphi@gmail.com>
2022-08-11 18:29:25 -0700

    Regenerate artifacts.

--- build.cmd
+++ build.cmd
@@ -25,14 +25,14 @@ IF NOT EXIST "%nuget_pkg_path%" (
   )
 )
 
-call :init evoskuil libbitcoin-system master
+call :init libbitcoin libbitcoin-system master
 IF %ERRORLEVEL% NEQ 0 (
-  call :failure "Initializing repository evoskuil libbitcoin-system master failed."
+  call :failure "Initializing repository libbitcoin libbitcoin-system master failed."
   exit /b 1
 )
-call :init evoskuil libbitcoin-protocol master
+call :init libbitcoin libbitcoin-protocol master
 IF %ERRORLEVEL% NEQ 0 (
-  call :failure "Initializing repository evoskuil libbitcoin-protocol master failed."
+  call :failure "Initializing repository libbitcoin libbitcoin-protocol master failed."
   exit /b 1
 )
 call :bld_repo libbitcoin-client
